13431447aa7d0f78a2d99c7fb26f4c7d88dfe707
Vedant Lohbare <vedant.lohbare@gmail.com>
2022-03-29 13:22:42 +0530

    Closes #1

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
+import { GlobalStyles } from "../styles/GlobalStyles";
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
+      <GlobalStyles />
       <Component {...pageProps} />
     </>
   );
